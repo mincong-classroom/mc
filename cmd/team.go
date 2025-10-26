92d0b64c8e194c25f9dde384eb64c87a74ab3d06
Mincong Huang <mincong.h@gmail.com>
2025-10-26 06:53:41 +0100

    update

--- cmd/team.go
+++ cmd/team.go
@@ -10,6 +10,8 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+const year = 2025
+
 var teamCmd = &cobra.Command{
 	Use:   "team",
 	Short: "List all teams",
@@ -30,7 +32,7 @@ func runTeam(cmd *cobra.Command, args []string) {
 
 // listTeams returns a list of team names by reading the classroom directory
 func listTeams() ([]common.Team, error) {
-	teamFile := fmt.Sprintf("%s/.mc/teams.yaml", os.Getenv("HOME"))
+	teamFile := fmt.Sprintf("%s/.mc/teams-%d.yaml", os.Getenv("HOME"), year)
 	teamData, err := os.ReadFile(teamFile)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read file: %v", err)
